ed0c0348eab4a30aaf8d925000edb9161701fe8e
real-musafir <alshahadath.com>
2025-07-22 08:27:27 +0600

    change home background color

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,10 +13,8 @@ const geistMono = Geist_Mono({
 
 export default function Home() {
   return (
-    <div
-      className={`${geistSans.className} ${geistMono.className} font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20`}
-    >
+    <main className="min-h-screen bg-black text-white font-sans">
       <h1>Home PAGE</h1>
-    </div>
+    </main>
   );
 }
