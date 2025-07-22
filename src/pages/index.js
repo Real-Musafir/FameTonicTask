a5f0849612ceb56e0ba49de63544a64d7a7d957d
real-musafir <alshahadath.com>
2025-07-22 09:29:22 +0600

    built navbar section

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,6 +22,39 @@ export default function Home() {
           <span className="">start your journey today!</span>
         </p>
       </section>
+
+      <nav className="w-full flex justify-between items-center px-6 py-4 bg-black">
+        <div className="text-2xl font-bold">
+          <span className="text-white">fame</span>
+          <span className="text-pink-500">tonic</span>
+        </div>
+        <div className="space-x-6 hidden md:flex">
+          <a href="#" className="hover:underline">
+            About us
+          </a>
+          <a href="#" className="hover:underline">
+            Contact
+          </a>
+        </div>
+        <div className="md:hidden">
+          <button>
+            <svg
+              className="w-6 h-6 text-white"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          </button>
+        </div>
+      </nav>
     </main>
   );
 }
