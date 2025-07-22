1368b036852049dbd9d249132563d2a415aef994
real-musafir <alshahadath.com>
2025-07-22 14:41:32 +0600

    aligned get started button section

--- src/pages/index.js
+++ src/pages/index.js
@@ -86,12 +86,14 @@ export default function Home() {
               ✨ Learn from expert-led courses designed for aspiring influencers
             </li>
           </ul>
-          <button className="mt-6 px-6 py-3 bg-pink-500 hover:bg-pink-600 text-white font-bold rounded-full shadow-md w-full md:w-auto">
-            GET STARTED <span className="ml-1">&gt;</span>
-          </button>
-          <p className="text-xs mt-2">
-            1-minute quiz for personalized insights
-          </p>
+          <div className=" w-full md:w-2/3">
+            <button className="mt-6 mb-1  w-full px-10 py-2 bg-pink-600 hover:bg-pink-500 text-white font-bold rounded-md drop-shadow-[4px_4px_6px_rgba(34,211,238,0.8)]">
+              GET STARTED <span className="ml-1">&gt;</span>
+            </button>
+            <p className="text-xs text-center mt-1">
+              1-minute quiz for personalized insights
+            </p>
+          </div>
           <p className="text-xs text-gray-400">
             By clicking "Get Started", you agree with Terms and Conditions,
             Privacy Policy, Subscription Terms
